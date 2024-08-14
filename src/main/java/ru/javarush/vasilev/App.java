7a2786b2a7008a94e0de12346dcaa0b9d68c325e
sergey <enzzomatrix@gmail.com>
2024-08-14 22:43:33 +1100

    upd

---
+++ src/main/java/ru/javarush/vasilev/App.java
@@ -0,0 +1,20 @@
+package ru.javarush.vasilev;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+import ru.javarush.vasilev.Entity.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    public static void main( String[] args ){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        try(Session session = sessionFactory.openSession()){
+            NativeQuery nativeQuery = session.createNativeQuery("CREATE TABLE persons");
+        }
+    }
+}
